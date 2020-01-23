ad69b73cd727dd66e017c90d0471175144cbfc44
Lighting225 <lighting225@ymail.com>
2020-01-23 17:05:16 -0500

    Revert "test"
    This reverts commit 8a11beac4cae85f5b9075a2724934852f0cd53c4.

--- main.js
+++
@@ -1,6 +0,0 @@
-const toggleButton = document.getElementsByClassName('navBar')[0];
-const navLinks = document.getElementsByClassName('Nav-links')[0];
-
-toggleButton.addEventListener('click', () => {
-  navLinks.classList.toggle('active');
-});
